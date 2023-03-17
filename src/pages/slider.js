cd4580e1fbb74b4696f1ea0dea2905bf56e4a758
Saad <asaad5436@gmail.com>
2023-03-18 02:07:53 +0500

    Slider finished -autoscroll

--- src/pages/slider.js
+++ src/pages/slider.js
@@ -1,57 +1,50 @@
 import React from "react"
-
-class Lider extends React.Component {
-    constructor(){
-        super()
-        this.scroll = this.scroll.bind(this)
-    }
-
-
-    scroll(direction){
-        let far = $( '.image-container' ).width()/2*direction;
-        let pos = $('.image-container').scrollLeft() + far;
-        $('.image-container').animate( { scrollLeft: pos }, 1000)
-    }
-
-    render() {
-        return <div className="main">
-            <div className="wrapper">
-                <div className="image-container">
-                    <div className="image"><img src="/carousel/image1.jpg" alt="image-1"/></div>
-                    <div className="image"><img src="/carousel/image2.gif" alt="image-2"/> </div>
-                    <div className="image"><img src="/carousel/image3.jpg" alt="image-3"/></div>
-                    <div className="image"><img src="/carousel/image4.jpg" alt="image-4"/></div>
-                    <div className="image"><img src="/carousel/image5.jpg" alt="image-5"/></div>
-                    <div className="image"><img src="/carousel/image6.jpg" alt="image-6"/></div>
-                    <div className="image"><img src="/carousel/image1.jpg" alt="image-1"/></div>
-                    <div className="image"><img src="/carousel/image2.gif" alt="image-2"/> </div>
-                    <div className="image"><img src="/carousel/image3.jpg" alt="image-3"/></div>
-                    <div className="image"><img src="/carousel/image4.jpg" alt="image-4"/></div>
-                    <div className="image"><img src="/carousel/image5.jpg" alt="image-5"/></div>
-                    <div className="image"><img src="/carousel/image6.jpg" alt="image-6"/></div>
-                    <div className="image"><img src="/carousel/image7.jpg" alt="image-7"/></div>
-                    <div className="image"><img src="/carousel/image1.jpg" alt="image-1"/></div>
-                    <div className="image"><img src="/carousel/image2.gif" alt="image-2"/> </div>
-                    <div className="image"><img src="/carousel/image3.jpg" alt="image-3"/></div>
-                    <div className="image"><img src="/carousel/image4.jpg" alt="image-4"/></div>
-                    <div className="image"><img src="/carousel/image5.jpg" alt="image-5"/></div>
-                    <div className="image"><img src="/carousel/image6.jpg" alt="image-6"/></div>
-                    <div className="image"><img src="/carousel/image7.jpg" alt="image-7"/></div>
-                    <div className="image"><img src="/carousel/image1.jpg" alt="image-1"/></div>
-                    <div className="image"><img src="/carousel/image2.gif" alt="image-2"/> </div>
-                    <div className="image"><img src="/carousel/image3.jpg" alt="image-3"/></div>
-                    <div className="image"><img src="/carousel/image4.jpg" alt="image-4"/></div>
-                    <div className="image"><img src="/carousel/image5.jpg" alt="image-5"/></div>
-                    <div className="image"><img src="/carousel/image6.jpg" alt="image-6"/></div>
-                    <div className="image"><img src="/carousel/image7.jpg" alt="image-7"/></div>
-                    <div className="image"><img src="/carousel/image7.jpg" alt="image-7"/></div>
-                </div>
-            </div>
-        </div>;
-    }
-}
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 
 export default function Slider(){
-    return  <Lider />;
+    return  <div className="slider-papi" >
+       <Splide
+      options={{
+        type: "loop",
+        gap: "90px",
+        drag: "free",
+        arrows: false,
+        pagination: false,
+        perPage: 3,
+        fixedWidth: '400px',
+        fixedHeight: '400px',
+        autoScroll: {
+          pauseOnHover: false,
+          pauseOnFocus: false,
+          rewind: false,
+          speed: 1
+        }
+      }}
+      extensions={{ AutoScroll }}>
+      <SplideSlide>
+          <img src="/carousel/image1.jpg" alt="Image 1" />
+      </SplideSlide>
+      <SplideSlide>
+          <img src="/carousel/image2.gif" alt="Image 2" />
+      </SplideSlide>
+      <SplideSlide>
+          <img src="/carousel/image5.jpg" alt="Image 3" />
+      </SplideSlide>
+      <SplideSlide>
+          <img src="/carousel/image4.jpg" alt="Image 4" />
+      </SplideSlide>
+      <SplideSlide>
+          <img src="/carousel/image3.jpg" alt="Image 5" />
+      </SplideSlide>
+      <SplideSlide>
+          <img src="/carousel/image6.jpg" alt="Image 6" />
+      </SplideSlide>
+      <SplideSlide>
+          <img src="/carousel/image7.jpg" alt="Image 6" />
+      </SplideSlide>
+    </Splide>
+</div>
 }
--- src/pages/vidplayer.js
+++ src/pages/vidplayer.js
@@ -1,4 +1,7 @@
 import {Container} from "@mui/material" ;
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 export default function vid(){
     return <div className="vid-outer"> 
