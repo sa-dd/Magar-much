0204450b2de9b01f16c32cfa777571dd98cd63f1
Saad <asaad5436@gmail.com>
2023-03-24 00:29:14 +0500

    half footer done

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,4 +1,6 @@
-import Box from '@mui/material/Box'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebook, faInstagram, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons'
+
 export default function Contact(){
     return <div className='footer-mam'>
         <div className="footer-contact">
@@ -12,13 +14,28 @@ export default function Contact(){
                     <span className="box1-para"> 
         Very maza ayega buht maza aya humny khaana khaya hahaha!!
         Very maza ayega buht maza aya humny khaana khaya hahaha!! 
-         Very maza ayega buht maza aya humny khaana khaya hahaha!! 
+        Very maza ayega buht maza aya humny khaana khaya hahaha!! 
     </span>
-                    <span className="box1-sign"> Sign Up Now</span>
-                </div>
+    <span className="box1-sign"> Sign Up Now</span>
+</div>
 
             </div>
             <div className="contact2-box2">
+
+                <div className="box2"> 
+                    <span className="box2-heading"> Connect With <span> Us </span> </span>
+                    <span className="box2-icons">
+                        <span> <FontAwesomeIcon icon={faFacebook} /> </span>
+                        <span><FontAwesomeIcon icon={faInstagram} /> </span>
+                    <span><FontAwesomeIcon icon={faTwitter} /> </span>
+                <span><FontAwesomeIcon icon={faYoutube} /> </span>
+                    </span>
+                    <span className="box2-copyright"> 
+                        <span> Delivery powered by CD 70 </span>
+                        <span> Copyright © 2023 Magar Munchh Pakistan </span>
+                    </span>
+                </div>
+
 
             </div>
         </div>
