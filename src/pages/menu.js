355096fc64eb7e73199d0b1df2760e0c22ed39cb
Saad <asaad5436@gmail.com>
2023-04-20 07:26:33 +0500

    fetch items based on category

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -18,11 +18,12 @@ export async function getServerSideProps() {
 }
 
 export default function About(props) {
-     const [selectedOption, setSelectedOption] = useState('featured');
+    console.log(props.items);
+    const [selectedOption, setSelectedOption] = useState('featured');
 
-  const handleOption= (event) => {
-    setSelectedOption(event.target.value);
-  }
+    const handleOption= (event) => {
+        setSelectedOption(event.target.value);
+    }
     return <div className='about-cont'>
         <div className='section-holder'>
             <div className='section1'>
@@ -44,44 +45,50 @@ export default function About(props) {
                 </div>
 
                 <div className='food-section'>
-                    <label className='fs1'>
-                    <input type='radio' name='featured' value='featured' checked={selectedOption === 'featured'} onChange={handleOption}/>
+                    <input type='radio' id='featured' name='featured' value='Featured' checked={selectedOption === 'Featured'} onChange={handleOption}/>
+                    <label className='fs1'for='featured'>
                         <img src='/sec2.png' alt='image-1' />
                         <span className='fs1-text'> Featured </span>
-                </label>
-                    <label className='fs1' href=''> 
+                    </label>
 
-                    <input type='radio' name='breakfast' value='breakfast' checked={selectedOption === 'breakfast'}onChange={handleOption}/>
+                    <input type='radio' id='breakfast' name='breakfast' value='Breakfast' checked={selectedOption === 'Breakfast'}onChange={handleOption}/>
+
+                    <label className='fs1' for='breakfast'>
                         <img src='/sec6.png' alt='image-2' />
                         <span className='fs1-text'> Breakfast</span>
                     </label>
-                    <label className='fs1' href=''> 
 
-                    <input type='radio' name='bundles' value='bundles'checked={selectedOption === 'bundles'} onChange={handleOption}/>
+                    <input type='radio' name='bundles' id='bundles'value='Bundles'checked={selectedOption === 'Bundles'} onChange={handleOption}/>
+
+                    <label className='fs1' for='bundles'> 
                         <img src='/sec9.png' alt='image-3' />
                         <span className='fs1-text'> Bundles </span>
                     </label>
-                    <label className='fs1' href='#'> 
 
-                    <input type='radio' name='drinks' value='drinks' checked={selectedOption === 'drinks'}onChange={handleOption}/>
+                    <input type='radio' id='drinks' name='drinks' value='Drinks' checked={selectedOption === 'Drinks'}onChange={handleOption}/>
+
+                    <label className='fs1' for='drinks'> 
                         <img src='/sec10.png' alt='image-4' />
                         <span className='fs1-text'> Drinks</span>
                     </label>
-                    <label className='fs1' href='#'> 
 
-                    <input type='radio' name='sides' value='sides' checked={selectedOption === 'sides'}onChange={handleOption}/>
+                    <input type='radio' id='sides' name='sides' value='Sides' checked={selectedOption === 'Sides'}onChange={handleOption}/>
+
+                    <label className='fs1' for='sides'> 
                         <img src='/sec8.png' alt='image-4' />
                         <span className='fs1-text'>Sides</span>
                     </label>
-                    <label className='fs1' href='#'> 
 
-                    <input type='radio' name='cafe' value='cafe' checked={selectedOption === 'cafe'}onChange={handleOption}/>
+                    <input type='radio' name='cafe' id='cafe' value='Cafe' checked={selectedOption === 'Cafe'}onChange={handleOption}/>
+
+                    <label className='fs1' for='cafe'> 
                         <img src='/sec1.png' alt='image-4' />
                         <span className='fs1-text'>M's Cafe</span>
                     </label>
-                    <label className='fs1' href='#'> 
 
-                    <input type='radio' name='dessert' value='dessert' checked={selectedOption === 'dessert'}onChange={handleOption}/>
+                    <input type='radio' name='dessert' id='dessert' value='Dessert' checked={selectedOption === 'Dessert'}onChange={handleOption}/>
+
+                    <label className='fs1' for='dessert'> 
                         <img src='/sec3.png' alt='image-4' />
                         <span className='fs1-text'>Dessert</span>
                     </label>
@@ -91,23 +98,24 @@ export default function About(props) {
                     <div className='food-cont'>
         {
             props.items.map((item, index)=>(
-                <div className='item-cont' key={index}>
-                    <div className='item-cont-text'>
-                        <span className='item-name'> {item.Name}</span>
-                        <span className='item-desc'> {item.Description} </span>
-                        <span className='item-price'> Price: <span className='ips'>${item.Price}</span> </span>
+                (item.Category === selectedOption)? (
+                    <div className='item-cont' key={index}>
+                        <div className='item-cont-text'>
+                            <span className='item-name'> {item.Name}</span>
+                            <span className='item-desc'> {item.Description} </span>
+                            <span className='item-price'> Price: <span className='ips'>${item.Price}</span> </span>
+                        </div>
+                        <img src={`http://localhost:8080${item.Image}`} alt= "burger"/>
                     </div>
-                    <img src={`http://localhost:8080${item.Image}`} alt= "burger"/>
-                </div>
-
-
-            ))
+                ) : null
+            )
+            )
         }
 
     </div>
         {/* <div className='cart'> hi </div> */}
+    </div>
 </div>
-            </div>
         </div>
 
         <Footer />
